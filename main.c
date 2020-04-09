d43bdb4f9f9cb352e4f93ae5c929d27eadc06772
Jordan Savant <md5madman@gmail.com>
2020-04-09 18:27:15 -0500

    saviet

--- main.c
+++ main.c
@@ -1,3 +1,37 @@
+#include <ncurses.h>
+
+enum GameState
+{
+	gs_start,
+	gs_intro,
+	gs_main,
+	gs_exit
+};
+
+void g_setup() {
+	// setup ncurses
+	initscr();
+}
+
+void g_teardown() {
+	// teardown ncurses
+	endwin();
+}
+
 int main(void) {
+	// Game Loop
+	// Intro
+	// Main Loop
+	// Exit
+	enum GameState state = gs_start;
+
+	g_setup();
+
+	while (state != gs_exit) {
+	}
+
+	g_teardown();
+
 	return 0;
 }
+
