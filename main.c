034fbd91c2099bd760d8fd18c80f4d42687f11d6
Jordan Savant <md5madman@gmail.com>
2020-04-09 19:58:44 -0500

    intro

--- main.c
+++ main.c
@@ -1,33 +1,108 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <ncurses.h>
 
-enum GameState
+#define SCOLOR_NORMAL 1
+#define TCOLOR_NORMAL 2
+#define TCOLOR_OMINOUS 3
+
+enum game_state
 {
-	gs_start,
-	gs_intro,
-	gs_main,
-	gs_exit
+	GS_START,
+	GS_INTRO,
+	GS_MAIN,
+	GS_EXIT
 };
 
-void g_setup() {
+bool g_setup()
+{
 	// setup ncurses
 	initscr();
+	if (has_colors()) {
+		start_color();
+	} else {
+		printf("%s\n", "Colors not supported on this terminal");
+		endwin(); // cleanup
+		return false;
+	}
+	// colors
+	init_pair(SCOLOR_NORMAL, COLOR_WHITE, COLOR_BLACK);
+	init_pair(TCOLOR_NORMAL, COLOR_WHITE, COLOR_BLACK);
+	init_pair(TCOLOR_OMINOUS, COLOR_RED, COLOR_BLACK);
+
+	// set primary color
+	bkgd(COLOR_PAIR(SCOLOR_NORMAL));
+	return true;
 }
 
-void g_teardown() {
+void g_teardown()
+{
 	// teardown ncurses
 	endwin();
 }
 
-int main(void) {
-	// Game Loop
-	// Intro
-	// Main Loop
-	// Exit
-	enum GameState state = gs_start;
+void center(int row, char *title, int colorpair, bool bold)
+{
+	if (bold)
+		attrset(COLOR_PAIR(colorpair) | A_BOLD);
+	else
+		attrset(COLOR_PAIR(colorpair));
+	int len, indent, y, width;
+	getmaxyx(stdscr, y, width);	/* get screen width */
+	len = strlen(title);		/* get title's length */
+	indent = width - len;		/* subtract it from screen width */
+	indent /= 2;			/* divide result into two */
+	mvaddstr(row, indent, title);
+	attrset(COLOR_PAIR(SCOLOR_NORMAL));
+}
 
-	g_setup();
+enum intro_state
+{
+	INTRO_1,
+	INTRO_2,
+	INTRO_3,
+	INTRO_4,
+	INTRO_EXIT
+};
+void g_intro()
+{
+	curs_set(0); // hide cursor
+	enum intro_state state = INTRO_1;
 
-	while (state != gs_exit) {
+	center(3, "It was an age of light.", TCOLOR_NORMAL, 0);
+	refresh(); getch();
+	center(4, "It was an age of hope.", TCOLOR_NORMAL, false);
+	refresh(); getch();
+	center(6, "And then he came...", TCOLOR_OMINOUS, true);
+	refresh(); getch();
+	clear();
+	center(6, "And then he came...", TCOLOR_OMINOUS, true);
+	refresh(); getch();
+	center(6, "And then he came...", TCOLOR_OMINOUS, false);
+	refresh(); getch();
+
+	curs_set(1);
+}
+
+
+int main(void)
+{
+	if (!g_setup()) {
+		return 1;
+	}
+
+	enum game_state state = GS_START;
+	while (state != GS_EXIT) {
+		switch (state) {
+		case GS_START:
+			state = GS_INTRO;
+			break;
+		case GS_INTRO:
+			g_intro();
+			state = GS_EXIT;
+			break;
+		}
 	}
 
 	g_teardown();
