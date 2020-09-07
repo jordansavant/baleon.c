ef6f4de90e5a77fd6cfafc5bbfb031246b49b4bf
Jordan Savant <md5madman@gmail.com>
2020-09-07 11:40:19 -0500

    test func pointer

---
+++ ref/x_funcpoint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void test_function(int (*function_pointer) (int x)) {
+	printf("addr passed function_pointer %p\n", function_pointer);
+	if (function_pointer(100)) {
+		printf("  run: true\n");
+	} else {
+		printf("  run: false\n");
+	}
+}
+
+int function_outside_main(int x)
+{
+	return x < 0;
+}
+
+int main(void)
+{
+	// run with function defined globally
+	printf("addr function_outside_main %p\n", function_outside_main);
+	test_function(function_outside_main);
+
+	// run with function defined in this stack block
+	int function_inside_main(int x) {
+		return x > 0;
+	}
+	printf("addr function_inside_main %p\n", function_inside_main);
+	test_function(function_inside_main); // shouldn't the address be valid?
+}
